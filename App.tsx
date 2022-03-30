41138c14893e9207ad2f8b5bd02cd952a7f675bd
bavly19 <bavly abdelmasih>
2022-03-30 20:16:54 +0200

    creating news screen files

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet} from 'react-native'
 import React from 'react'
+import News from './src/screens/News'
 
 const App = () => {
   return (
     <SafeAreaView style={styles.containerStyle}>
-      <Text>App</Text>
+      <News />
     </SafeAreaView>
   )
 }
